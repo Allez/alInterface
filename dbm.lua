3705e135b94efa1ef9743e6a99a275d8c6a5182e
Allez <p_alexey@hotbox.ru>
2010-10-18 15:54:02 +0400

    default dbm options

--- dbm.lua
+++ dbm.lua
@@ -1,4 +1,4 @@
-if not IsAddOnLoaded("DBM-Core") or not DBM then return end
+if not IsAddOnLoaded("DBM-Core") then return end
 
 local backdrop = {
 	bgFile = [=[Interface\ChatFrame\ChatFrameBackground]=],
@@ -17,7 +17,7 @@ local CreateBG = function(parent)
 	return bg
 end
 
-local function SkinBars(self)
+local SkinBars = function(self)
 	for bar in self:GetBarIterator() do
 		if not bar.injected then
 			bar.ApplyStyle=function()
@@ -58,11 +58,19 @@ local function SkinBars(self)
 
 				if not icon1.styled then
 					icon1:SetTexCoord(0.07, 0.93, 0.07, 0.93)
+					icon1.frame = CreateFrame("Frame", nil, tbar)
+					icon1.frame:SetFrameStrata("BACKGROUND")
+					icon1.frame:SetAllPoints(icon1)
+					icon1.frame.background = CreateBG(icon1.frame)
 					icon1.styled=true
 				end
 
 				if not icon2.styled then
 					icon2:SetTexCoord(0.07, 0.93, 0.07, 0.93)
+					icon2.frame = CreateFrame("Frame", nil, tbar)
+					icon2.frame:SetFrameStrata("BACKGROUND")
+					icon2.frame:SetAllPoints(icon2)
+					icon2.frame.background = CreateBG(icon2.frame)
 					icon2.styled=true
 				end
 
@@ -94,4 +102,38 @@ local function SkinBars(self)
 	end
 end
 
+local UploadDBM = function()
+	DBM_SavedOptions.Enabled = true
+	DBM_SavedOptions.ShowMinimapButton = false
+	DBM_SavedOptions.WarningIconLeft = false
+	DBM_SavedOptions.WarningIconRight = false
+	DBM_SavedOptions.RangeFrameX = 244
+	DBM_SavedOptions.RangeFramePoint = "LEFT"
+	DBM_SavedOptions.ShowSpecialWarnings = true
+
+	DBT_SavedOptions["DBM"].Scale = 1
+	DBT_SavedOptions["DBM"].HugeScale = 1
+	DBT_SavedOptions["DBM"].BarXOffset = 0
+	DBT_SavedOptions["DBM"].BarYOffset = 5
+	DBT_SavedOptions["DBM"].Font = 'Fonts\\VisitorR.TTF'
+	DBT_SavedOptions["DBM"].FontSize = 10
+	DBT_SavedOptions["DBM"].Width = 150
+	DBT_SavedOptions["DBM"].TimerX = 28
+	DBT_SavedOptions["DBM"].TimerY = 250
+	DBT_SavedOptions["DBM"].TimerPoint = "BOTTOMLEFT"
+	DBT_SavedOptions["DBM"].FillUpBars = true
+	DBT_SavedOptions["DBM"].IconLeft = true
+	DBT_SavedOptions["DBM"].ExpandUpwards = true
+	DBT_SavedOptions["DBM"].Texture = "Interface\\TargetingFrame\\UI-StatusBar"
+	DBT_SavedOptions["DBM"].IconRight = false
+	DBT_SavedOptions["DBM"].HugeBarXOffset = 0
+	DBT_SavedOptions["DBM"].HugeBarsEnabled = false
+end
+
 hooksecurefunc(DBT, "CreateBar", SkinBars)
+
+local frame = CreateFrame("Frame")
+frame:RegisterEvent('PLAYER_LOGIN')
+frame:SetScript('OnEvent', function(self, event)
+	UploadDBM()
+end)
