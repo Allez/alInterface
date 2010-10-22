11db82e8a2f4b3908c74bdf2d4c730e6c2b02b17
Allez <p_alexey@hotbox.ru>
2010-10-22 15:01:36 +0400

    now works in test mode

--- config.lua
+++ config.lua
@@ -4,25 +4,24 @@ UIConfig = {
 		spacing = 3,
 	},
 	DamageMeter = {
-		anchor = "TOPLEFT"
-		x, y = 12, -12
-		barheight = 14
-		spacing = 1
-		maxbars = 8
-		width, height = 125, maxbars*(barheight+spacing)-spacing
-		maxfights = 10
-		reportstrings = 10
-		texture = "Interface\\TargetingFrame\\UI-StatusBar"
-		backdrop_color = {0, 0, 0, 0.5}
-		border_color = {0, 0, 0, 1}
-		border_size = 1
-		font = 'Fonts\\VisitorR.TTF'
-		font_size = 10
-		hidetitle = false
+		anchor = "TOPLEFT",
+		x = 12,
+		barheight = 14,
+		spacing = 1,
+		maxbars = 8,
+		width = 150,
+		maxfights = 10,
+		reportstrings = 10,
+		texture = "Interface\\TargetingFrame\\UI-StatusBar",
+		border_size = 1,
+		font = 'Fonts\\VisitorR.TTF',
+		font_size = 10,
+		hidetitle = false,
 	},
 }
 
 local UISetup = {}
+local lastVisible = nil
 local backdrop = {
 	bgFile = [=[Interface\ChatFrame\ChatFrameBackground]=],
 	edgeFile = [=[Interface\ChatFrame\ChatFrameBackground]=], edgeSize = 1,
@@ -49,7 +48,7 @@ end
 
 local CreateConfigFrame = function()
 	local main = CreateFrame("Frame", "UIConfigFrame", UIParent)
-	local scroll = CreateFrame("ScrollFrame", "UIConfigScrollFrame", main, "UIParentScrollFrameTemplate")
+	local scroll = CreateFrame("ScrollFrame", "UIConfigScrollFrame", main)--, "UIParentScrollFrameTemplate")
 	local groups = CreateFrame("Frame", "UIConfigGroupFrame", main)
 	local set = CreateFrame("Button", "UIConfigSetFrame", main)
 	local cancel = CreateFrame("Button", "UIConfigCancelFrame", main)
@@ -59,12 +58,17 @@ local CreateConfigFrame = function()
 	main.bg = CreateBG(main)
 	scroll:SetSize(150, 550)
 	scroll:SetPoint("BOTTOMLEFT", 12, 12)
-	scroll:SetScrollChild(group)
+	scroll:SetScrollChild(groups)
 	scroll.bg = CreateBG(scroll)
+	groups:SetAllPoints(scroll)
 
 	set.bg = CreateBG(set)
-	set.SetPoint("TOPRIGHT", main, "BOTTOMRIGHT", 0, -5)
-	set:SetScript("OnMouseUp", function()
+	set.label = set:CreateFontString(nil, "OVERLAY", "GameFontNormal")
+	set.label:SetPoint("CENTER")
+	set.label:SetText(ACCEPT)
+	set:SetSize(50, 20)
+	set:SetPoint("TOPRIGHT", main, "BOTTOMRIGHT", 0, -5)
+	set:SetScript("OnClick", function()
 		for group, options in pairs(UISetup) do
 			for option, value in pairs(options) do
 				UIConfig[group][option] = value
@@ -73,8 +77,13 @@ local CreateConfigFrame = function()
 		ReloadUI()
 	end)
 	cancel.bg = CreateBG(cancel)
+	cancel.label = cancel:CreateFontString(nil, "OVERLAY", "GameFontNormal")
+	cancel.label:SetPoint("CENTER")
+	cancel.label:SetText(CANCEL)
+	cancel:SetSize(50, 20)
 	cancel:SetPoint("RIGHT", set, "LEFT", -5, 0)
-	cancel:SetScript("OnMouseUp", function()
+	cancel:SetScript("OnClick", function()
+		wipe(UISetup)
 		_G["UIConfigFrame"]:Hide()
 	end)
 
@@ -85,11 +94,20 @@ local CreateConfigFrame = function()
 		button:SetPoint("TOP", 0, -offset-1)
 		local label = button:CreateFontString(nil, "OVERLAY")
 		label:SetPoint("CENTER")
-		label:SetFont(GetFontNormal:GetFont(), 14)
-		local setings = CreateFrame("Frame", "UIConfigSettings"..group, main)
-		settings:SetSize(600, 576)
-		settings:SetPoint("RIGHT", -12, 0)
+		label:SetFont(GameFontNormal:GetFont(), 14)
+		label:SetText(group)
+		local settings = CreateFrame("Frame", "UIConfigSettings"..group, main)
+		settings:SetSize(600, 550)
+		settings:SetPoint("BOTTOMRIGHT", -12, 12)
 		settings.bg = CreateBG(settings)
+		settings:Hide()
+		button:SetScript("OnMouseUp", function()
+			if lastVisible then
+				lastVisible:Hide()
+			end
+			settings:Show()
+			lastVisible = settings
+		end)
 		offset = offset + 21
 		local offsetgroup = 5
 		for option, value in pairs(options) do
@@ -103,29 +121,33 @@ local CreateConfigFrame = function()
 			if type(value) == "number" or type(value) == "string" then
 				local label = settings:CreateFontString(nil, "OVERLAY", "GameFontHighlight")
 				label:SetText(option)
-				label:SetWidth(300)
+				label:SetWidth(200)
 				label:SetHeight(20)
 				label:SetJustifyH("LEFT")
 				label:SetPoint("TOPLEFT", 5, -(offsetgroup))
 				local editbox = CreateFrame("EditBox", nil, settings)
 				editbox:SetAutoFocus(false)
 				editbox:SetMultiLine(false)
-				editbox:SetWidth(200)
+				editbox:SetWidth(300)
 				editbox:SetHeight(20)
 				editbox:SetMaxLetters(255)
 				editbox:SetTextInsets(3, 0, 0, 0)
 				editbox:SetFontObject(GameFontHighlight)
 				editbox:SetPoint("LEFT", label, "RIGHT", 5, 0)
 				editbox:SetText(value)
-				editbox.bg = CreateBG(editbox)
-				local okbutton = CreateFrame("Button", nil, frame)
+				editbox:SetBackdrop(backdrop)
+				editbox:SetBackdropColor(0, 0, 0, 0)
+				editbox:SetBackdropBorderColor(0.2, 0.2, 0.2, 1)
+				local okbutton = CreateFrame("Button", nil, settings)
 				okbutton:SetHeight(editbox:GetHeight())
-				okbutton:SetWidth(editbox:GetHeight() + 5)
-				okbutton.bg = CreateBG(okbutton)
-				okbutton:SetPoint("LEFT", editbox, "RIGHT", 2, 0)
-				okbutton:Hide()
+				okbutton:SetWidth(55)
+				okbutton:SetBackdrop(backdrop)
+				okbutton:SetBackdropColor(0, 0, 0, 0)
+				okbutton:SetBackdropBorderColor(0.2, 0.2, 0.2, 1)
+				okbutton:SetPoint("LEFT", editbox, "RIGHT", 5, 0)
+				--okbutton:Hide()
 				local oktext = okbutton:CreateFontString(nil, "OVERLAY", "GameFontNormal")
-				oktext:SetText(OK)
+				oktext:SetText(ACCEPT)
 				oktext:SetPoint("CENTER")
  
 				editbox:SetScript("OnEscapePressed", function(self) okbutton:Hide() self:ClearFocus() self:SetText(value) end)
@@ -137,8 +159,12 @@ local CreateConfigFrame = function()
 					editbox:SetScript("OnEnterPressed", function(self) okbutton:Hide() self:ClearFocus() SetValue(group,option,tostring(self:GetText())) end)
 					okbutton:SetScript("OnMouseDown", function(self) editbox:ClearFocus() self:Hide() SetValue(group,option,tostring(editbox:GetText())) end)
 				end
-			else
+			end
 			offsetgroup = offsetgroup + 25
 		end
 	end
 end
+
+
+SlashCmdList["UICONFIG"] = CreateConfigFrame
+SLASH_UICONFIG1 = "/uiconfig"
