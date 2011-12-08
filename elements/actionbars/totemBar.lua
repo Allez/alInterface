1826fe24f1017ddb0f3daf76f2537d27b6636011
Allez <p_alexey@hotbox.ru>
2011-12-08 13:14:47 +0400

    TotemBar style

--- elements/actionbars/totemBar.lua
+++ elements/actionbars/totemBar.lua
@@ -4,6 +4,11 @@ local bar = CreateBar("uiTotemBar")
 bar:SetPoint("BOTTOMLEFT", 12, 210)
 bar.rows = 1
 bar.buttons = {}
+tinsert(bar.buttons, _G["MultiCastSummonSpellButton"])
+for i = 1, NUM_MULTI_CAST_BUTTONS_PER_PAGE do
+	tinsert(bar.buttons, _G["MultiCastSlotButton"..i])
+end
+tinsert(bar.buttons, _G["MultiCastRecallSpellButton"])
 
 if MultiCastActionBarFrame then
 	MultiCastActionBarFrame:SetScript("OnUpdate", nil)
@@ -12,11 +17,12 @@ if MultiCastActionBarFrame then
 	MultiCastActionBarFrame:SetParent(bar)
 	MultiCastActionBarFrame:ClearAllPoints()
 	MultiCastActionBarFrame:SetPoint("BOTTOMLEFT", bar, 0, 0)
-	for i = 1, 4 do
-		local b = _G["MultiCastSlotButton"..i]
-		local b2 = _G["MultiCastActionButton"..i]
-		b:ClearAllPoints()
-		b:SetAllPoints(b2)
+	local index = 1
+	for i = 1, NUM_MULTI_CAST_PAGES do
+		for j = 1, NUM_MULTI_CAST_BUTTONS_PER_PAGE do
+			_G["MultiCastActionButton"..index]:SetAllPoints(_G["MultiCastSlotButton"..j])
+		end
+		index = index + 1
 	end
 	MultiCastActionBarFrame.SetParent = function() end
 	MultiCastActionBarFrame.SetPoint = function() end
