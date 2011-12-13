9d73331584c2205f15405664d5566a2cfc4ac602
Allez <p_alexey@hotbox.ru>
2011-12-13 22:23:27 +0400

    capture bar fix and move

--- elements/capture.lua
+++ elements/capture.lua
@@ -1,26 +1,49 @@
 
 -- Skin Capture Bar
 
+local anchorframe = CreateFrame("Frame", "Capture Bar", UIParent)
+anchorframe:SetSize(130, 15)
+anchorframe:SetPoint("TOP", 0, -130)
+if UIMovableFrames then tinsert(UIMovableFrames, anchorframe) end
+
 local CaptureUpdate = function()
-	if not NUM_EXTENDED_UI_FRAMES then return end
 	for i = 1, NUM_EXTENDED_UI_FRAMES do
 		local bar = _G["WorldStateCaptureBar"..i]
 
-		if bar and bar:IsVisible() and not bar.skinned then
-			local name = bar:GetName()
+		if bar and bar:IsVisible() then
+			bar:ClearAllPoints()
+			if i == 1 then
+				bar:SetPoint("TOP", anchorframe, "TOP")
+			else
+				bar:SetPoint("TOPLEFT", _G["WorldStateCaptureBar"..i-1], "BOTTOMLEFT", 0, -7)
+			end
 
-			_G[name.."LeftLine"]:SetAlpha(0)
-			_G[name.."RightLine"]:SetAlpha(0)
-			_G[name.."LeftIconHighlight"]:SetAlpha(0)
-			_G[name.."RightIconHighlight"]:SetAlpha(0)
-			_G[name.."LeftBar"]:SetTexture("Interface\\TargetingFrame\\UI-StatusBar")
-			_G[name.."RightBar"]:SetTexture("Interface\\TargetingFrame\\UI-StatusBar")
-			_G[name.."MiddleBar"]:SetTexture("Interface\\TargetingFrame\\UI-StatusBar")
+			if not bar.skinned then
+				local name = bar:GetName()
 
-			select(4, bar:GetRegions()):Hide()
+				left = _G[name.."LeftBar"]
+				right = _G[name.."RightBar"]
+				middle = _G[name.."MiddleBar"]
+				_G[name.."LeftLine"]:SetAlpha(0)
+				_G[name.."RightLine"]:SetAlpha(0)
+				_G[name.."LeftIconHighlight"]:SetAlpha(0)
+				_G[name.."RightIconHighlight"]:SetAlpha(0)
+				left:SetTexture("Interface\\TargetingFrame\\UI-StatusBar")
+				right:SetTexture("Interface\\TargetingFrame\\UI-StatusBar")
+				middle:SetTexture("Interface\\TargetingFrame\\UI-StatusBar")
 
-			CreateBG(bar)
-			bar.skinned = true
+				select(4, bar:GetRegions()):Hide()
+				
+				left:SetVertexColor(0.2, 0.6, 1)
+				right:SetVertexColor(0.9, 0.2, 0.2)
+				middle:SetVertexColor(0.8, 0.8, 0.8)
+
+				bar.bg = CreateBG(bar)
+				bar.bg:SetPoint("TOPLEFT", _G[name.."LeftBar"], -2, 2)
+				bar.bg:SetPoint("BOTTOMRIGHT", _G[name.."RightBar"], 2, -2)
+
+				bar.skinned = true
+			end
 		end
 	end
 end
