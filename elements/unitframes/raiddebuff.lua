87989c7de154d5a4c9f44f81a7d15871eb46eb30
Allez <p_alexey@hotbox.ru>
2012-03-06 21:28:34 +0400

    update raid debuffs

--- elements/unitframes/raiddebuff.lua
+++ elements/unitframes/raiddebuff.lua
@@ -5,42 +5,39 @@ if not ORD then return end
 
 ORD.ShowDispelableDebuff = true
 ORD.FilterDispellableDebuff = true
-ORD.MatchBySpellName = false
+ORD.MatchBySpellName = true
 ORD.SHAMAN_CAN_DECURSE = true
 
 local debuffFilter = {
 -- "Vault of Archavon"
 	--Koralon
-	67332,66684,--Flaming Cinder (10, 25)
+	67332,--Flaming Cinder (10, 25)
 
 	--Toravon the Ice Watcher
-	72004,72098,72120,72121,--Frostbite
-
-	--Toravon the Ice Watcher
-	72004,72098,72120,72121,--Frostbite
+	72004,--Frostbite
 
 -- "Naxxramas"
 	--Trash
 	55314,--Strangulate
 
 	--Anub'Rekhan
-	28786, 54022,--Locust Swarm (N, H)
+	28786,--Locust Swarm (N, H)
 
 	--Grand Widow Faerlina
-	28796, 54098,--Poison Bolt Volley (N, H)
-	28794, 54099,--Rain of Fire (N, H)
+	28796,--Poison Bolt Volley (N, H)
+	28794,--Rain of Fire (N, H)
 
 	--Maexxna
 	28622,--Web Wrap (NH)
-	54121, 28776,--Necrotic Poison (N, H)
+	54121,--Necrotic Poison (N, H)
 
 	--Noth the Plaguebringer
-	29213, 54835,--Curse of the Plaguebringer (N, H)
-	29214, 54836,--Wrath of the Plaguebringer (N, H)
+	29213,--Curse of the Plaguebringer (N, H)
+	29214,--Wrath of the Plaguebringer (N, H)
 	29212,--Cripple (NH)
 
 	--Heigan the Unclean
-	29998, 55011,--Decrepit Fever (N, H)
+	29998,--Decrepit Fever (N, H)
 	29310,--Spell Disruption (NH)
 
 	--Grobbulus
@@ -51,15 +48,15 @@ local debuffFilter = {
 	29306,--Infected Wound (NH)
 
 	--Thaddius
-	28084, 28085,--Negative Charge (N, H)
-	28059, 28062,--Positive Charge (N, H)
+	28084,--Negative Charge (N, H)
+	28059,--Positive Charge (N, H)
 
 	--Instructor Razuvious
 	55550,--Jagged Knife (NH)
 
 	--Sapphiron
 	28522,--Icebolt (NH)
-	28542, 55665,--Life Drain (N, H)
+	28542,--Life Drain (N, H)
 
 	--Kel'Thuzad
 	28410,--Chains of Kel'Thuzad (H)
@@ -68,8 +65,8 @@ local debuffFilter = {
 
 -- "The Eye of Eternity"
 	--Malygos
-	56272, 60072,--Arcane Breath (N, H)
-	57407, 60936,--Surge of Power (N, H)
+	56272,--Arcane Breath (N, H)
+	57407,--Surge of Power (N, H)
 
 -- "The Obsidian Sanctum"
 	--Trash
@@ -82,38 +79,38 @@ local debuffFilter = {
 
 -- "Ulduar"
 	--Trash
-	62310, 62928,--Impale (N, H)
-	63612, 63673,--Lightning Brand (N, H)
+	62310,--Impale (N, H)
+	63612,--Lightning Brand (N, H)
 	63615,--Ravage Armor (NH)
-	62283, 62438,--Iron Roots (N, H)
-	63169, 63549,--Petrify Joints (N, H)
+	62283,--Iron Roots (N, H)
+	63169,--Petrify Joints (N, H)
 
 	--Razorscale
 	64771,--Fuse Armor (NH)
 
 	--Ignis the Furnace Master
-	62548, 63476,--Scorch (N, H)
-	62680, 63472,--Flame Jet (N, H)
-	62717, 63477,--Slag Pot (N, H)
+	62548,--Scorch (N, H)
+	62680,--Flame Jet (N, H)
+	62717,--Slag Pot (N, H)
 
 	--XT-002
-	63024, 64234,--Gravity Bomb (N, H)
-	63018, 65121,--Light Bomb (N, H)
+	63024,--Gravity Bomb (N, H)
+	63018,--Light Bomb (N, H)
 
 	--The Assembly of Iron
-	61888, 64637,--Overwhelming Power (N, H)
-	62269, 63490,--Rune of Death (N, H)
-	61903, 63493,--Fusion Punch (N, H)
-	61912, 63494,--Static Disruption(N, H)
+	61888,--Overwhelming Power (N, H)
+	62269,--Rune of Death (N, H)
+	61903,--Fusion Punch (N, H)
+	61912,--Static Disruption(N, H)
 
 	--Kologarn
-	64290, 64292,--Stone Grip (N, H)
-	63355, 64002,--Crunch Armor (N, H)
+	64290,--Stone Grip (N, H)
+	63355,--Crunch Armor (N, H)
 	62055,--Brittle Skin (NH)
 
 	--Hodir
 	62469,--Freeze (NH)
-	61969, 61990,--Flash Freeze (N, H)
+	61969,--Flash Freeze (N, H)
 	62188,--Biting Cold (NH)
 
 	--Thorim
@@ -121,12 +118,12 @@ local debuffFilter = {
 	62130,--Unbalancing Strike (NH)
 	62526,--Rune Detonation (NH)
 	62470,--Deafening Thunder (NH)
-	62331, 62418,--Impale (N, H)
+	62331,--Impale (N, H)
 
 	--Freya
 	62532,--Conservator's Grip (NH)
-	62589, 63571,--Nature's Fury (N, H)
-	62861, 62930,--Iron Roots (N, H)
+	62589,--Nature's Fury (N, H)
+	62861,--Iron Roots (N, H)
 
 	--Mimiron
 	63666,--Napalm Shell (N)
@@ -148,7 +145,7 @@ local debuffFilter = {
 	63042,--Dominate Mind (H)
 	64152,--Draining Poison (H)
 	64153,--Black Plague (H)
-	64125, 64126,--Squeeze (N, H)
+	64125,--Squeeze (N, H)
 	64156,--Apathy (H)
 	64157,--Curse of Doom (H)
 	--63050,--Sanity(NH)
@@ -158,34 +155,34 @@ local debuffFilter = {
 
 -- "Trial of the Crusader"
 	--Gormok the Impaler
-	66331, 67477, 67478, 67479,--Impale(10, 25, 10H, 25H)
+	66331,--Impale
 	66406,--Snobolled!
 
 	--Acidmaw --Dreadscale
-	66819, 67609, 67610, 67611,--Acidic Spew (10, 25, 10H, 25H)
-	66821, 67635, 67636, 67637,--Molten Spew (10, 25, 10H, 25H)
-	66823, 67618, 67619, 67620,--Paralytic Toxin (10, 25, 10H, 25H)
+	66819,--Acidic Spew
+	66821,--Molten Spew
+	66823,--Paralytic Toxin
 	66869,--Burning Bile
 
 	--Icehowl
-	66770, 67654, 67655, 67656,--Ferocious Butt(10, 25, 10H, 25H)
-	66689, 67650, 67651, 67652,--Arctic Breathe(10, 25, 10H, 25H)
+	66770,--Ferocious Butt
+	66689,--Arctic Breathe
 	66683,--Massive Crash
 
 	--Lord Jaraxxus
-	66532, 66963, 66964, 66965,--Fel Fireball (10, 25, 10H, 25H)
-	66237, 67049, 67050, 67051,--Incinerate Flesh (10, 25, 10H, 25H)
-	66242, 67059, 67060, 67061,--Burning Inferno (10, 25, 10H, 25H)
-	66197, 68123, 68124, 68125,--Legion Flame (10, 25, 10H, 25H)
-	66199, 68126, 68127, 68128,--Legion Flame (Patch?: 10, 25, 10H, 25H)
-	66877, 67070, 67071, 67072,--Legion Flame (Patch Icon?: 10, 25, 10H, 25H)
+	66532,--Fel Fireball 
+	66237,--Incinerate Flesh 
+	66242,--Burning Inferno
+	66197,--Legion Flame
+	66199,--Legion Flame
+	66877,--Legion Flame
 	66283,--Spinning Pain Spike
 	66209,--Touch of Jaraxxus(H)
 	66211,--Curse of the Nether(H)
 	66333, 66334, 66335, 66336, 68156,--Mistress' Kiss (10H, 25H)
 
 	--Faction Champions
-	65812, 68154, 68155, 68156,--Unstable Affliction (10, 25, 10H, 25H)
+	65812,--Unstable Affliction
 	65801,--Polymorph
 	65543,--Psychic Scream
 	66054,--Hex
@@ -194,14 +191,14 @@ local debuffFilter = {
 	--The Twin Val'kyr
 	67176,--Dark Essence
 	67223,--Light Essence
-	67282, 67283,--Dark Touch
-	67297, 67298,--Light Touch
-	67309, 67310, 67311, 67312,--Twin Spike (10, 25, 10H, 25H)
+	67282,--Dark Touch
+	67297,--Light Touch
+	67309,--Twin Spike
 
 	--Anub'arak
 	67574,--Pursued by Anub'arak
-	66013, 67700, 68509, 68510,--Penetrating Cold (10, 25, 10H, 25H)
-	67847, 67721,--Expose Weakness
+	66013,--Penetrating Cold (10, 25, 10H, 25H)
+	67847,--Expose Weakness
 	66012,--Freezing Slash
 	67863,--Acid-Drenched Mandibles(25H)
 
@@ -217,7 +214,7 @@ local debuffFilter = {
 	71204,--Touch of Insignificance
 	67934,--Frost Fever
 	71237,--Curse of Torpor
-	72491,71951,72490,72491,72492,--Necrotic Strike
+	72491,--Necrotic Strike
 
 	--Gunship Battle
 	69651,--Wounding Strike
@@ -229,47 +226,47 @@ local debuffFilter = {
 	72769,--Scent of Blood (heroic)
 
 	--Festergut
-	69290,71222,73033,73034,--Blighted Spore
-	69248,72274,--Vile Gas?
-	71218,72272,72273,73020,73019,69240,--Vile Gas?
-	72219,72551,72552,72553,--Gastric Bloat
-	69278,69279,71221, -- Gas Spore
+	69290,--Blighted Spore
+	69248,--Vile Gas?
+	71218,--Vile Gas?
+	72219,--Gastric Bloat
+	69278, -- Gas Spore
 
 	--Rotface
-	69674,71224,73022,73023,--Mutated Infection
+	69674,--Mutated Infection
 	69508,--Slime Spray
-	30494,69774,69776,69778,71208,--Sticky Ooze
+	69774,--Sticky Ooze
 
 	--Professor Putricide
-	70672,72455,72832,72833,--Gaseous Bloat
+	70672,--Gaseous Bloat
 	72549,--Malleable Goo
 	72454,--Mutated Plague
 	70341,--Slime Puddle (Spray)
-	70342,70346,72869,72868,--Slime Puddle (Pool)
-	70911,72854,72855,72856,--Unbound Plague
-	69774,72836,72837,72838,--Volatile Ooze Adhesive
+	70342,--Slime Puddle (Pool)
+	70911,--Unbound Plague
+	69774,--Volatile Ooze Adhesive
 
 	--Blood Prince Council
-	71807,72796,72797,72798,--Glittering Sparks
-	71911,71822,--Shadow Resonance
+	71807,--Glittering Sparks
+	71911,--Shadow Resonance
 
 	--Blood-Queen Lana'thel
-	71623,71624,71625,71626,72264,72265,72266,72267,--Delirious Slash
+	71623,--Delirious Slash
 	70949,--Essence of the Blood Queen (hand icon)
-	70867,70871,70872,70879,70950,71473,71525,71530,71531,71532,71533,--Essence of the Blood Queen (bite icon)
-	72151,72648,72650,72649,--Frenzied Bloodthirst (bite icon)
-	71474,70877,--Frenzied Bloodthirst (red bite icon)
-	71340,71341,--Pact of the Darkfallen
+	70867,--Essence of the Blood Queen (bite icon)
+	72151,--Frenzied Bloodthirst (bite icon)
+	71474,--Frenzied Bloodthirst (red bite icon)
+	71340,--Pact of the Darkfallen
 	72985,--Swarming Shadows (pink icon)
-	71267,71268,72635,72636,72637,--Swarming Shadows (black purple icon)
-	71264,71265,71266,71277,72638,72639,72640,72890,--Swarming Shadows (swirl icon)
-	70923,70924,73015,--Uncontrollable Frenzy
+	71267,--Swarming Shadows (black purple icon)
+	71264,--Swarming Shadows (swirl icon)
+	70923,--Uncontrollable Frenzy
 
 	--Valithria Dreamwalker
 	70873,--Emerald Vigor
-	70744,71733,72017,72018,--Acid Burst
-	70751,71738,72021,72022,--Corrosion
-	70633,71283,72025,72026,--Gut Spray
+	70744,--Acid Burst
+	70751,--Corrosion
+	70633,--Gut Spray
 
 	--Sindragosa
 	70106,--Chilled to the Bone
@@ -277,12 +274,12 @@ local debuffFilter = {
 	69762,--Unchained Magic
 	70126,--Frost Beacon
 	71665,--Asphyxiation
-	70127,72528,72529,72530,--Mystic Buffet
+	70127,--Mystic Buffet
 
 	--Lich King
-	70541,73779,73780,73781,--Infest
-	70337,70338,73785,73786,73787,73912,73913,73914,--Necrotic Plague
-	72133,73788,73789,73790,--Pain and Suffering
+	70541,--Infest
+	70337,--Necrotic Plague
+	72133,--Pain and Suffering
 	68981,--Remorseless Winter
 	69242,--Soul Shriek
 
@@ -291,7 +288,7 @@ local debuffFilter = {
 	69483,--Dark Reckoning
 	71163,--Devour Humanoid
 	71127,--Mortal Wound
-	70435,71154,--Rend Flesh
+	70435,--Rend Flesh
 	
 -- "The Ruby Sanctum"
 	--Baltharus the Warborn
@@ -425,37 +422,48 @@ local debuffFilter = {
 	91325,	-- Shadow Vortex
 -- Dragon Soul
 	-- Morchok
-	103687,	-- Crush Armor
-	103536,	-- Warning
-	103534,	-- Danger
-	108570,	-- Black Blood of the Earth
-	-- Warlord Zon'ozz
-	103434,	-- Disrupting Shadows
-	-- Yor'sahj the Unsleeping
-	105171,	-- Deep Corruption
+	103687, -- Crush Armor
 	-- Hagara the Stormbinder
-	104451,	-- Ice Tomb
-	109325,	-- Frostflake
-	105289,	-- Shattered Ice
-	105285,	-- Target
-	107061,	-- Ice Lance
+	104451,  -- Ice Tomb
+	105285,  -- Target (next Ice Lance)
+	105316,  -- Ice Lance
+	105289,  -- Shattered Ice
+	105259,  -- Watery Entrenchment	
+	105465,  -- Lightning Storm
+	105369,  -- Lightning Conduit
+	- Warmaster Blackhorn
+	109204, -- Twilight Barrage
+	108046, -- Shockwave
+	108043, -- Devastate
+	107567, -- Brutal strike
+	107558, -- Degeneration
+	110214, -- Consuming Shroud
 	-- Ultraxion
-	110080,	-- Fading Light
-	110070,	-- Fading Light
-	-- Warmaster Blackhorn
-	108043,	-- Devastate
-	108046,	-- Shockwave
-	107567,	-- Brutal Strike
-	107558,	-- Degeneration
+	110068, -- Fading light 
+	106108, -- Heroic will
+	106415, -- Twilight burst
+	105927, -- Faded Into Twilight
+	106369, -- Twilight shift
+	-- Yor'sahj the Unsleeping
+	104849, -- Void bolt
+	109389, -- Deep Corruption
+	-- Warlord Zon'ozz
+	103434, -- Disrupting shadows
+	110306, -- Black Blood of Go'rath
 	-- Spine of Deathwing
-	105563,	-- Grasping Tendrils
-	105479,	-- Searing Plasma
-	105490,	-- Fiery Grip
+	105563, -- Grasping Tendrils
+	105490, -- Fiery Grip
+	105479, -- Searing Plasma
+	106199, -- Blood corruption: death
+	106200, -- Blood corruption: earth
+	106005, -- Degradation
 	-- Madness of Deathwing
-	105841,	-- Degenerative bite
-	105445,	-- Blistering heat
-	109603,	-- Tetanus
-	110141,	-- Shrapnel
+	109603, -- Tetanus
+	109632, -- Impale
+	106794, -- Shrapnel
+	106385, -- Crush
+	105841, -- Degenerative bite
+	105445, -- Blistering heat
 }
 
 ORD:RegisterDebuffs(debuffFilter)
